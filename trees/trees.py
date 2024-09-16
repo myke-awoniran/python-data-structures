ee28765cdc35dcad2d908d7e350c9a40fbb99f83
Michael Awoniran <awoniranopeyemi@gmail.com>
2024-09-16 05:34:52 +0100

    update to BST

--- trees/trees.py
+++ trees/trees.py
@@ -1,15 +1,17 @@
-class TreeNode:
+class Node:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
+
 class BinarySearchTree:
     def __init__(self):
         self.root = None
 
+
     def insert(self, value):
-        new_node = TreeNode(value)
+        new_node = Node(value)
         if self.root is None:
             self.root = new_node
             return True
@@ -19,10 +21,35 @@ class BinarySearchTree:
                 if temp.left is None:
                     temp.left = new_node
                     return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        # if root is none return False
+        # temp= self.root
+        # while temp is None:
+        # update temp on each loop
+
+        temp = self.root
+        while temp:
+            if temp.value < value:
+                temp = temp.left
+            elif temp.value > value:
+                temp = temp.right
+            else:
+                return True
+        return False
 
 
 
 
-my_tree= BinarySearchTree()
-print(my_tree.root)
-
+my_tree = BinarySearchTree()
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+print(my_tree.contains(7))
+print(my_tree.root.right.value)
