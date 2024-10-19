92077f1438a617985337a5ebfe460410474f5cd5
Myke Awoniran <awoniranopeyemi@gmail.com>
2024-10-19 14:50:22 +0100

    add graph interfaces

--- graphs/graph.py
+++ graphs/graph.py
@@ -4,7 +4,7 @@ class Graph:
 
     def print_graph(self):
         for vertex in self.adjacency_list:
-            print(self.adjacency_list[vertex])
+            print(vertex, ':', self.adjacency_list[vertex])
 
     def add_vertex(self, vertex):
         if vertex not in self.adjacency_list.keys():
@@ -14,9 +14,27 @@ class Graph:
 
     def add_edge(self, vertex1, vertex2):
         graph_list = self.adjacency_list
-        if graph_list[vertex1] in graph_list.keys() and graph_list[vertex2] in graph_list.keys():
+        if vertex1 in graph_list.keys() and vertex2 in graph_list.keys():
             graph_list[vertex1].append(vertex2)
             graph_list[vertex2].append(vertex1)
+            return True
+        return False
+
+    def remove_edge(self, vertex1, vertex2):
+        adjacency_list = self.adjacency_list
+        if vertex1 in adjacency_list.keys() and vertex2 in adjacency_list.keys():
+            adjacency_list[vertex1].remove(vertex2)
+            adjacency_list[vertex2].remove(vertex1)
+            return True
+        return False
+
+    def remove_vertex(self, vertex):
+        adjacency_list = self.adjacency_list
+        if vertex in adjacency_list.keys():
+            for other_vertex in adjacency_list[vertex]:
+                adjacency_list[other_vertex].remove(vertex)
+            # adjacency_list.pop(vertex)
+            del adjacency_list[vertex]
             return True
         return False
 
@@ -24,7 +42,10 @@ class Graph:
 my_graph = Graph()
 my_graph.add_vertex('a')
 my_graph.add_vertex('b')
-my_graph.add_edge('a', 'b')
 my_graph.add_vertex('c')
+my_graph.add_edge('a', 'b')
+my_graph.add_edge('b', 'c')
+my_graph.remove_edge('a', 'b')
+my_graph.remove_vertex('b')
 my_graph.add_vertex('d')
 my_graph.print_graph()
